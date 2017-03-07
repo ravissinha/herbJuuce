610f6c129064db591f5dd0ccd685f4be1a603974
ravi shankar <ravi.shankar1@sial.com>
2017-03-08 02:43:18 +0530

    angular project set up

---
+++ mzeun-ui/app/app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var mZeun = angular.module('mZeun', []);
+
+mZeun.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+        when('/Stores', {
+            templateUrl: 'view/storeView.html',
+            controller: 'StoreViewController'
+        }).
+        when('/Orders', {
+            templateUrl: 'view/orderView.html',
+            controller: 'OrderViewController'
+        }).
+        when('/Login', {
+            templateUrl: 'view/loginView.html'
+        }).
+        when('/progress', {
+            templateUrl: 'view/progressView.html',
+            controller: 'ProgressViewController'
+        }).
+        otherwise({
+            redirectTo: '/Stores'
+        });
+    }]);
+
+
+mZeun.controller('StoreViewController', function($scope) {
+
+    $scope.message = 'Store screen';
+
+});
+
+
+mZeun.controller('OrderViewController', function($scope) {
+
+    $scope.message = 'orders screen';
+
+});
+
+mZeun.controller('ProgressViewController', function($scope) {
+
+    $scope.message = 'In Progress';
+
+});
