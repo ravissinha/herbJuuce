ca4602f8ef687c464a70b1a99b013d7731ef53ce
Badal Sharma <badal.sharma@hp.com>
2017-03-17 14:22:55 +0530

    cart chnages

--- mzeun-ui/app/app.js
+++ mzeun-ui/app/app.js
@@ -1,34 +1,44 @@
 'use strict';
 
-var mZeun = angular.module('mZeun', []);
 
-mZeun.config(['$routeProvider',
-    function($routeProvider) {
-        $routeProvider.
-        when('/Stores', {
-            templateUrl: 'view/storeView.html',
-            controller: 'StoreViewController'
-        }).
-        when('/Orders', {
-            templateUrl: 'view/orderView.html',
-            controller: 'OrderViewController'
-        }).
-        when('/Login', {
-            templateUrl: 'view/loginView.html'
-        }).
-        when('/progress', {
-            templateUrl: 'view/progressView.html',
-            controller: 'ProgressViewController'
-        }).
-        when('/cart',{
-        templateUrl: 'htmls/shoppingCart.htm',
-        controller:   storeController
-        }).
-        otherwise({
-            redirectTo: '/Stores'
-        });
-    }]);
+var mZeun = angular.module('mZeun', []).
+  config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+      when('/store', {
+        templateUrl: 'view/store.htm',
+        controller: storeController 
+      }).
+      when('/products/:productSku', {
+        templateUrl: 'view/product.htm',
+        controller: storeController
+      }).
+      when('/cart', {
+        templateUrl: 'view/shoppingCart.htm',
+        controller: storeController
+      }).
+      otherwise({
+        redirectTo: '/store'
+      });
+}]);
 
+
+mZeun.factory("DataService", function () {
+
+    // create store
+    var myStore = new store();
+
+    // create shopping cart
+    var myCart = new shoppingCart("mZeun");
+
+
+    myCart.addCheckoutParameters("PayPal", "bernardo.castilho-facilitator@gmail.com");
+
+    // return data object with store and cart
+    return {
+        store: myStore,
+        cart: myCart
+    };
+});
 
 mZeun.controller('StoreViewController', function($scope) {
 
